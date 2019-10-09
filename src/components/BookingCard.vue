ad01a0de3bf3e57037824c3bb9b0e41d6503b715
chriztiaan <chriz.ek@gmail.com>
2019-10-09 22:39:20 +0200

    Made an informative Booking card.

---
+++ src/components/BookingCard.vue
@@ -0,0 +1,41 @@
+<template>
+	<v-card>
+		<v-card-title>Booking </v-card-title>
+		<v-card-text>
+			<v-row class="black--text" no-gutters="">
+				<span>16:30 <v-icon color="secondary">mdi-clock-outline</v-icon></span>
+				<v-spacer></v-spacer>
+				<span>5 <v-icon color="secondary">mdi-account-multiple</v-icon></span>
+			</v-row>
+			This is a regular client that would like to be waitered by Kevin.
+		</v-card-text>
+		<v-card-actions class="pr-1">
+			<v-row no-gutters>
+				<v-col class="pb-0" cols="12" sm="8">
+					<v-avatar>
+						<v-img
+							class="elevation-2"
+							src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+						></v-img>
+					</v-avatar>
+					Christiaan Landman
+				</v-col>
+				<v-col class="pb-0" cols="8" offset="4" offset-sm="0" sm="4">
+					<span class="d-flex " :class="{ 'float-right': !$vuetify.breakpoint.xs }">
+						<v-btn class="ma-0" icon x-large>
+							<v-icon color="success">mdi-check-circle-outline</v-icon>
+						</v-btn>
+						<v-btn class="ma-0" icon x-large>
+							<v-icon color="error">mdi-close-circle-outline</v-icon>
+						</v-btn>
+					</span>
+				</v-col>
+			</v-row>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({});
+</script>
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,13 +1,28 @@
 <template>
 	<div>
-		a
+		<v-container>
+			<v-row>
+				<v-col cols="12" md="6" lg="4">
+					<BookingCard></BookingCard>
+				</v-col>
+				<v-col cols="12" md="6" lg="4">
+					<BookingCard></BookingCard>
+				</v-col>
+				<v-col cols="12" md="6" lg="4">
+					<BookingCard></BookingCard>
+				</v-col>
+			</v-row>
+		</v-container>
 	</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import BookingCard from './BookingCard.vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+	components: { BookingCard }
+});
 </script>
 
 <style scoped></style>
